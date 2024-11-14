0cbeee7a8f508c8ca5238dc48377030eada42adb
israel carvalho <161456671+raelc23@users.noreply.github.com>
2024-11-14 08:46:31 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'academia',
+    'O que é academia ?',
+    'e um sentro de treinamento que te ensina a cuidar do seu corpo '
+)
+
+criaCartao(
+    'treinos ',
+    'oque voce alcança com os treinos ?',
+    'voce alcança confiança e força e dedicaçao cada dia mais '
+)
+
+criaCartao(
+    'alimentaçao',
+    'o que e dieta ?',
+    'Dieta é o conjunto de alimentos e bebidas que uma pessoa consome regularmente.'
+)
+
+criaCartao(
+    'objetivo',
+    'qual sao os objetivo das pessoas na academia ?',
+    'algumas pessoas querem ser saudavel e outras querem competir tipo fisiculturismo'
+)
+criaCartao(
+    'academia ',
+    'a academia faz mal para idosos ?',
+    'nao e ainda melhor para saude deles '
+)
+
+criaCartao(
+    'fisiculturismo',
+    'o que e fisiculturismo?',
+    'Fisiculturismo é uma prática esportiva e de treinamento focada no desenvolvimento muscular e na definição do corpo.'
+)
